945a02ec813c354e2421d5945cbda44e8fe5e5a7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2020-04-17 17:29:11 +0100

    Initial Commit

---
+++ GeoBus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeoBus
+//
+//  Created by João on 17/04/2020.
+//  Copyright © 2020 João. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
