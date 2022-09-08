30c560fe01c70f08ea30d11e9173e7a85368f116
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-09-08 19:59:14 +0100

    Indentation changes

--- GeoBus/ContentView.swift
+++ GeoBus/ContentView.swift
@@ -11,53 +11,53 @@ import Combine
 import MapKit
 
 struct ContentView : View {
-  
-  @Environment(\.colorScheme) var colorScheme: ColorScheme
-  
-  @ObservedObject var routesStorage = RoutesStorage()
-  @ObservedObject var vehiclesStorage = VehiclesStorage()
-  
-  @State var mapView = MKMapView()
-  
-  @State var showSelectRouteSheet: Bool = false
-  
-  
-  var body: some View {
-    
-    VStack {
-      
-      ZStack(alignment: .topTrailing) {
-        
-        MapView(mapView: $mapView, routesStorage: routesStorage, vehiclesStorage: vehiclesStorage)
-          .edgesIgnoringSafeArea(.vertical)
-          .padding(.bottom, -10)
-        
-        UserLocation(mapView: $mapView)
-        
-        if routesStorage.isStopSelected() {
-          StopDetails(
-            publicId: routesStorage.selectedStopAnnotation?.publicId ?? "",
-            name: routesStorage.selectedStopAnnotation?.name ?? "-",
-            orderInRoute: routesStorage.selectedStopAnnotation?.orderInRoute ?? -1,
-            direction: routesStorage.selectedStopAnnotation?.direction ?? .ascending,
-            isOpen: true
-          )
-            .padding()
-            .shadow(color: Color(.black).opacity(0.20), radius: 10, x: 0, y: 0)
-        }
-        
+
+   @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+   @ObservedObject var routesStorage = RoutesStorage()
+   @ObservedObject var vehiclesStorage = VehiclesStorage()
+
+   @State var mapView = MKMapView()
+
+   @State var showSelectRouteSheet: Bool = false
+
+
+   var body: some View {
+
+      VStack {
+
+         ZStack(alignment: .topTrailing) {
+
+            MapView(mapView: $mapView, routesStorage: routesStorage, vehiclesStorage: vehiclesStorage)
+               .edgesIgnoringSafeArea(.vertical)
+               .padding(.bottom, -10)
+
+            UserLocation(mapView: $mapView)
+
+            if routesStorage.isStopSelected() {
+               StopDetails(
+                  publicId: routesStorage.selectedStopAnnotation?.publicId ?? "",
+                  name: routesStorage.selectedStopAnnotation?.name ?? "-",
+                  orderInRoute: routesStorage.selectedStopAnnotation?.orderInRoute ?? -1,
+                  direction: routesStorage.selectedStopAnnotation?.direction ?? .ascending,
+                  isOpen: true
+               )
+               .padding()
+               .shadow(color: Color(.black).opacity(0.20), radius: 10, x: 0, y: 0)
+            }
+
+         }
+
+         HStack {
+            SelectRoute(routesStorage: routesStorage, vehiclesStorage: vehiclesStorage, showSelectRouteSheet: $showSelectRouteSheet)
+            RouteDetails(routesStorage: routesStorage, vehiclesStorage: vehiclesStorage, showSelectRouteSheet: $showSelectRouteSheet)
+            Spacer()
+         }
+         .frame(height: 115)
+         .background(colorScheme == .dark ? Color(.systemGray5) : Color(.white))
+
       }
-      
-      HStack {
-        SelectRoute(routesStorage: routesStorage, vehiclesStorage: vehiclesStorage, showSelectRouteSheet: $showSelectRouteSheet)
-        RouteDetails(routesStorage: routesStorage, vehiclesStorage: vehiclesStorage, showSelectRouteSheet: $showSelectRouteSheet)
-        Spacer()
-      }
-      .frame(height: 115)
-      .background(colorScheme == .dark ? Color(.systemGray5) : Color(.white))
-      
-    }
-    
-  }
-  
+
+   }
+
 }
