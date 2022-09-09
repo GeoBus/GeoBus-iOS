7fb5d2f7c519edd2fbd2fc8722b9edddf0341e6c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-09-09 14:31:13 +0100

    Simplified Authentication flow with Boutique @StoredValue

--- GeoBus/Authentication/Authentication.swift
+++ GeoBus/Authentication/Authentication.swift
@@ -7,14 +7,15 @@
 //
 
 import Foundation
+import Boutique
+import Combine
 
 class Authentication: ObservableObject {
 
    /* MARK: - Overview */
 
-   // 1. RETRIEVE SAVED CREDENTIAL FROM STORAGE
-   //    Check if there is already a saved Credential in iCloud KVS.
-   //    If found, use this. If not, retrieve a new one.
+   // 1. AUTHENTICATE
+   //    Orchestrate steps to ensure valid authentication flow.
 
    // 2. FETCH LATEST AVAILABLE CREDENTIAL FROM SERVER
    //    This improves reliability as valid credentials can become
@@ -25,214 +26,119 @@ class Authentication: ObservableObject {
    //    Fetch the Carris Authentication API to retrieve a valid authToken
    //    and refreshToken to process further requests.
 
-   /* * */
-
 
    /* MARK: - Variables */
 
    private var retries = 3
-   private var credential = Credential()
-   private var authorization = Authorization()
 
-   @Published var authToken = ""
+   @StoredValue(key: "endpoint")
+   var endpoint: String? = nil
 
-   /* * */
+   @StoredValue(key: "apiKey")
+   var apiKey: String? = nil
 
+   @StoredValue(key: "authToken")
+   var authToken: String? = nil
 
-   /* MARK: - Errors */
-
-   enum AuthenticationError: Error {
-      case InvalidCredentialFromKVS
-      case CredentialServerFetchError
-      case CarrisAPIUnavailable
-      case CarrisAPIUnauthorizedAccess
-   }
-
-   /* * */
+   @StoredValue(key: "refreshToken")
+   var refreshToken: String? = nil
 
 
    /* MARK: - Init */
 
+   // Initiate authentication on init to save time.
+
    init() {
-      // Initiate authentication on init to save time
       authenticate()
    }
 
-   /* * */
 
+   /* MARK: - 1. AUTHENTICATE */
 
-   /* MARK: - 0. Authenticate */
-
-   // This function agregates all the steps required for authentication
+   // This function agregates all the steps required for authentication.
 
    func authenticate() {
 
-      if retries > 0 {
+      Task {
 
-         retries -= 1
-
-         Task {
-
-            // 1. CREDENTIAL
-
+         if (refreshToken != nil) {
             do {
-               // 1. If necessary,
-               if !(self.credential.isValid()) {
-                  // 1.A. Retrieve Credential from KVS
-                  print("Retrieving Credential from iCloud KVS...")
-                  self.credential = try retrieveCredentialFromKVS()
-               }
-
-            } catch AuthenticationError.InvalidCredentialFromKVS {
-               do {
-                  // 1.B. Retrieve Credential from Server
-                  print("KVS has no valid Credential. Fetching new from server...")
-                  self.credential = try await fetchLatestCredential()
-                  self.credential.saveToKVS()
-
-               } catch AuthenticationError.CredentialServerFetchError {
-                  // 1.C. Impossible to get valid credential
-                  print("Credential server is unavailable.")
-                  return
-               }
-            }
-
-
-            // 2. AUTHORIZATION
-
-            do {
-               // 2. Get authorization_token from Carris Auth API
-               print("Authorizing with Carris API...")
-               self.authorization = try await fetchAuthorization()
-
+               print("Authorizing with Carris API using refreshToken...")
+               try await fetchAuthorization(token: refreshToken!, type: "refresh")
             } catch {
-               // 2.2. The request was OK but the credential is not accepted
-               print("Available credential is invalid. Clearing everything...")
-               print(credential.token)
-               self.credential.clear()
-               self.authorization.clear()
+               print("Clearing saved refreshToken...")
+               self.$refreshToken.reset()
                self.authenticate()
                return
             }
-
+         } else if (apiKey != nil) {
+            do {
+               print("Authorizing with Carris API using apiKey...")
+               try await fetchAuthorization(token: apiKey!, type: "apikey")
+            } catch {
+               print("Clearing saved apiKey...")
+               self.$apiKey.reset()
+               self.authenticate()
+               return
+            }
+         } else {
+            do {
+               if (retries > 0) {
+                  print("Retrieving latest credential from server...")
+                  try await fetchLatestCredential()
+                  authenticate()
+                  retries -= 1
+                  return
+               }
+            } catch {
+               print("Unkown Error")
+               return
+            }
          }
 
       }
 
    }
 
-   /* * */
-
-
-   /* MARK: - 1. RETRIEVE SAVED CREDENTIAL FROM STORAGE */
-
-   // Retrieve previously saved Credential from iCloud KVS Storage
-
-   func retrieveCredentialFromKVS() throws -> Credential {
-      // Syncronize with iCloud KVS
-      let iCloudKeyStore = NSUbiquitousKeyValueStore()
-      iCloudKeyStore.synchronize()
-
-      // Retrieve from KVS and throw if invalid
-      guard let retrievedEndpoint = iCloudKeyStore.string(forKey: "credentialEndpoint") else {
-         throw AuthenticationError.InvalidCredentialFromKVS
-      }
-      guard let retrievedToken = iCloudKeyStore.string(forKey: "credentialToken") else {
-         throw AuthenticationError.InvalidCredentialFromKVS
-      }
-      guard let retrievedType = iCloudKeyStore.string(forKey: "credentialType") else {
-         throw AuthenticationError.InvalidCredentialFromKVS
-      }
-
-      // Return retrieved Credential to caller
-      return Credential(
-         endpoint: retrievedEndpoint,
-         token: retrievedToken,
-         type: retrievedType
-      )
-   }
-
-   /* * */
-
 
    /* MARK: - 2. FETCH LATEST AVAILABLE CREDENTIAL FROM SERVER */
 
    // Get latest available Credential from endpoint
 
-   func fetchLatestCredential() async throws -> Credential {
+   func fetchLatestCredential() async throws {
 
-      // Setup the request
       var request = URLRequest(url: URL(string: "https://joao.earth/api/geobus/carris_auth")!)
       request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
       request.httpMethod = "GET"
 
-      do {
-         let (data, _) = try await URLSession.shared.data(for: request)
-         let decodedCredential = try JSONDecoder().decode(Credential.self, from: data)
-         return Credential(
-            endpoint: decodedCredential.endpoint,
-            token: decodedCredential.token,
-            type: decodedCredential.type
-         )
-      } catch {
-         print("Error: \(error)")
-         throw AuthenticationError.CredentialServerFetchError
-      }
+      let (rawCredential, _) = try await URLSession.shared.data(for: request)
+      let decodedCredential = try JSONDecoder().decode(CredentialResponse.self, from: rawCredential)
+      self.$endpoint.set(decodedCredential.endpoint)
+      self.$apiKey.set(decodedCredential.token)
 
    }
-
-   /* * */
 
 
    /* MARK: - 3. GET VALID AUTH TOKEN AND REFRESH TOKEN FROM AUTHENTICATION API */
 
    // Get authorization tokens from Carris API
 
-   func fetchAuthorization() async throws -> Authorization {
+   func fetchAuthorization(token: String, type: String) async throws {
 
-      // Setup the request
-      var request = URLRequest(url: URL(string: "https://" + self.credential.endpoint + "/gateway/authenticationapi/authorization/sign")!)
+      var request = URLRequest(url: URL(string: "https://" + self.endpoint! + "/gateway/authenticationapi/authorization/sign")!)
       request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
       request.httpMethod = "POST"
 
+      request.httpBody = try JSONSerialization.data(withJSONObject: ["token": token, "type": type], options: .prettyPrinted)
 
-      // Define a new variable
-      var parameters: [String: Any]
+      let (data, _) = try await URLSession.shared.data(for: request)
 
-      if (self.authorization.isValid()) {
-         // Use Refresh Token
-         parameters = ["token": self.authorization.refreshToken, "type": "refresh"]
-      } else {
-         // Use Credential
-         parameters = ["token": self.credential.token, "type": self.credential.type]
-      }
+      let parsedAuthorization = try JSONDecoder().decode(AuthorizationResponse.self, from: data)
 
-      // Serialize parameters into JSON
-      request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-
-      // Perform request
-      let (data, response) = try await URLSession.shared.data(for: request)
-
-      // If response is not OK
-      let httpResponse = response as? HTTPURLResponse
-
-      if httpResponse?.statusCode == 400 {
-         // Retry because the refresh code might be expired
-         print("Request failed with status 400 - Unauthorized.")
-         throw AuthenticationError.CarrisAPIUnauthorizedAccess
-      } else if httpResponse?.statusCode != 200 {
-         print("Request failed for unknown reason.")
-         throw AuthenticationError.CarrisAPIUnavailable
-      }
-
-      let parsedAuthorization = try JSONDecoder().decode(Authorization.self, from: data)
-      self.authToken = parsedAuthorization.authorizationToken
-
-      return parsedAuthorization
+      self.$refreshToken.set(parsedAuthorization.refreshToken)
+      self.$authToken.set(parsedAuthorization.authorizationToken)
 
    }
-
-   /* * */
-
+   
 
 }
