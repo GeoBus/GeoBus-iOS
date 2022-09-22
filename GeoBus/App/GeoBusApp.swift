7b0fb940a746276fb5c32107528c82ede5560686
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-09-22 01:08:25 +0100

    Posthog changes

--- GeoBus/App/GeoBusApp.swift
+++ GeoBus/App/GeoBusApp.swift
@@ -19,8 +19,17 @@ struct GeoBusApp: App {
    init() {
       if let posthogApiKey = Bundle.main.infoDictionary?["POSTHOG_API_KEY"] as? String {
          let configuration = PHGPostHogConfiguration(apiKey: posthogApiKey)
-         configuration.captureApplicationLifecycleEvents = true;
-         configuration.recordScreenViews = true;
+         configuration.shouldUseLocationServices = false
+         configuration.flushAt = 1
+         configuration.flushInterval = 10
+         configuration.maxQueueSize = 1000
+         configuration.captureApplicationLifecycleEvents = false
+         configuration.shouldUseBluetooth = false
+         configuration.recordScreenViews = false
+         configuration.captureInAppPurchases = false
+         configuration.capturePushNotifications = false
+         configuration.captureDeepLinks = false
+         configuration.shouldSendDeviceID = true
          PHGPostHog.setup(with: configuration)
          self.posthog = PHGPostHog.shared()
       } else {
