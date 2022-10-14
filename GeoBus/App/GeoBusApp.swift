794192f0f4f356ff2ae8541936128b4c0550fa8a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-10-14 04:02:04 +0100

    Appstate must be an EnvironmentObject for it to work
    Pass along the shared instance and it works nice

--- GeoBus/App/GeoBusApp.swift
+++ GeoBus/App/GeoBusApp.swift
@@ -13,30 +13,27 @@ struct GeoBusApp: App {
    
    /* MARK: - GEOBUS */
    
+   @StateObject private var appstate = Appstate.shared
+   @StateObject private var mapController = MapController()
    @StateObject private var stopsController = StopsController()
    @StateObject private var routesController = RoutesController()
    @StateObject private var vehiclesController = VehiclesController()
    @StateObject private var estimationsController = EstimationsController()
-   
-   @StateObject private var mapController = MapController()
-   @StateObject private var carrisNetworkController = CarrisNetworkController()
    
    private let updateIntervalTimer = Timer.publish(every: 20 /* seconds */, on: .main, in: .common).autoconnect()
    
    var body: some Scene {
       WindowGroup {
          ContentView()
-            // OLD
+            .environmentObject(Appstate.shared)
+            .environmentObject(mapController)
             .environmentObject(stopsController)
             .environmentObject(routesController)
             .environmentObject(vehiclesController)
             .environmentObject(estimationsController)
-            // NEW
-            .environmentObject(self.mapController)
-//            .environmentObject(self.carrisNetworkController)
             .onAppear(perform: {
                // Update Carris network model
-//               self.carrisNetworkController.start()
+               self.routesController.update()
                // Capture app open
                Analytics.shared.capture(event: .App_Session_Start)
             })
@@ -44,7 +41,9 @@ struct GeoBusApp: App {
                // Capture session continuation
                Analytics.shared.capture(event: .App_Session_Ping)
                // Update vehicles on timer call
-               self.vehiclesController.update(scope: .summary)
+               Task {
+                  await vehiclesController.fetchVehiclesFromCarrisAPI()
+               }
             }
       }
    }
