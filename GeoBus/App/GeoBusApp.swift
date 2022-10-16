67471a3b1f3fb91cb88a093fbd21e3a473db15a1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-10-16 22:11:00 +0100

    Switch to unified CarrisNetworkController

--- GeoBus/App/GeoBusApp.swift
+++ GeoBus/App/GeoBusApp.swift
@@ -1,25 +1,13 @@
-//
-//  GeoBusApp.swift
-//  GeoBus
-//
-//  Created by João de Vasconcelos on 09/09/2022.
-//  Copyright © 2022 João de Vasconcelos. All rights reserved.
-//
-
 import SwiftUI
+
+/* MARK: - GEOBUS */
 
 @main
 struct GeoBusApp: App {
    
-   /* MARK: - GEOBUS */
-   
    @StateObject private var appstate = Appstate.shared
    @StateObject private var mapController = MapController()
-   
-   @StateObject private var stopsController = StopsController()
-   @StateObject private var routesController = RoutesController()
-   @StateObject private var vehiclesController = VehiclesController()
-   @StateObject private var estimationsController = EstimationsController()
+   @StateObject private var carrisNetworkController = CarrisNetworkController()
    
    private let updateIntervalTimer = Timer.publish(every: 20 /* seconds */, on: .main, in: .common).autoconnect()
    
@@ -28,23 +16,14 @@ struct GeoBusApp: App {
          ContentView()
             .environmentObject(appstate)
             .environmentObject(mapController)
-            .environmentObject(stopsController)
-            .environmentObject(routesController)
-            .environmentObject(vehiclesController)
-            .environmentObject(estimationsController)
+            .environmentObject(carrisNetworkController)
             .onAppear(perform: {
-               // Update Carris network model
-               self.routesController.update()
-               // Capture app open
-               Analytics.shared.capture(event: .App_Session_Start)
+               self.carrisNetworkController.start() // Update Carris network model
+               Analytics.shared.capture(event: .App_Session_Start) // Capture app open
             })
             .onReceive(updateIntervalTimer) { event in
-               // Capture session continuation
-               Analytics.shared.capture(event: .App_Session_Ping)
-               // Update vehicles on timer call
-               Task {
-                  await vehiclesController.fetchVehiclesFromCarrisAPI()
-               }
+               self.carrisNetworkController.update() // Update vehicles on timer call
+               Analytics.shared.capture(event: .App_Session_Ping) // Capture session continuation
             }
       }
    }
