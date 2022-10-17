11b58e3a50f3d2aa060d981404ac99e8a2cb7ff7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-10-17 22:20:49 +0100

    Change to enum

--- GeoBus/App/Controllers/Networks/Carris/CarrisAPISettings.swift
+++ GeoBus/App/Controllers/Networks/Carris/CarrisAPISettings.swift
@@ -6,20 +6,12 @@ import Foundation
 /* Define here the various properties required to connect or related to the Carris API. */
 
 
-final class CarrisAPISettings {
+enum CarrisAPISettings {
    
    /* * */
    /* MARK: - CARRIS API ENDPOINT */
    /* Use this URL string to connect to Carris API. */
    
    static let endpoint: String = "https://gateway.carris.pt/gateway/xtranpassengerapi/api/v2.11"
-   
-   
-   
-   /* * */
-   /* MARK: - INITIALIZER */
-   /* With a private init it is possible to prevent multiple instances of this class. */
-   
-   private init() { }
    
 }
