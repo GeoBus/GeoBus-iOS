0098f24c292bda37babdae7e3696fda561ff51fc
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-10-18 02:49:41 +0100

    Organized MapController; it is now a singleton

--- GeoBus/App/Controllers/MapController.swift
+++ GeoBus/App/Controllers/MapController.swift
@@ -12,23 +12,53 @@ import SwiftUI
 @MainActor
 class MapController: ObservableObject {
    
-   @Published var region = MKCoordinateRegion(
-      center: CLLocationCoordinate2D(latitude: 38.721917, longitude: -9.137732),
-      latitudinalMeters: 15000, longitudinalMeters: 15000
-   )
+   /* * */
+   /* MARK: - SECTION 1: SETTINGS */
+   /* Static settings for the Map view. */
+   
+   private let initialMapRegion = CLLocationCoordinate2D(latitude: 38.721917, longitude: -9.137732)
+   private let initialMapZoom = CLLocationDistance(15000)
+   
+   private let annotationsZoomMargin = 1.7 // The margin on the sides of the annotations
+   
+   
+   
+   /* * */
+   /* MARK: - SECTION 2: PUBLISHED PROPERTIES */
+   /* Here are all the @Published variables that can be consumed by the app views. */
+   
+   @Published var region = MKCoordinateRegion()
    
    @Published var locationManager = CLLocationManager()
    @Published var showLocationNotAllowedAlert: Bool = false
    
-   private var stopAnnotations: [GenericMapAnnotation] = []
-   private var connectionAnnotations: [GenericMapAnnotation] = []
    @Published var visibleAnnotations: [GenericMapAnnotation] = []
    
    
    
-   /* MARK: - MOVE MAP TO NEW COORDINATE REGION */
+   /* * */
+   /* MARK: - SECTION 3: SHARED INSTANCE */
+   /* To allow the same instance of this class to be available accross the whole app, */
+   /* we create a Singleton. More info here: https://www.hackingwithswift.com/example-code/language/what-is-a-singleton */
    
-   // Helper function to animate Map changing region.
+   static let shared = MapController()
+   
+   
+   
+   /* * */
+   /* MARK: - SECTION 4: INITIALIZER */
+   /* Protect the initializer to ensure only one instance of this class is created. */
+   /* Setup the initial map region on init. */
+   
+   private init() {
+      self.region = MKCoordinateRegion(center: initialMapRegion, latitudinalMeters: initialMapZoom, longitudinalMeters: initialMapZoom)
+   }
+   
+   
+   
+   /* * */
+   /* MARK: - SECTION 5: MOVE MAP TO NEW COORDINATE REGION */
+   /* Helper function to animate the Map changing region. */
    
    func moveMap(to newRegion: MKCoordinateRegion) {
       DispatchQueue.main.async {
@@ -40,9 +70,9 @@ class MapController: ObservableObject {
    
    
    
-   /* MARK: - CENTER MAP ON USER LOCATION */
-   
-   // .....
+   /* * */
+   /* MARK: - SECTION 6: CENTER MAP ON USER LOCATION */
+   /* Lorem ipsum dolor sit amet consectetur adipisicing elit. */
    
    func centerMapOnUserLocation(andZoom: Bool) {
       
@@ -70,19 +100,51 @@ class MapController: ObservableObject {
    
    
    
-   /* MARK: - UPDATE ANNOTATIONS WITH SELECTED STOP */
+   /* * */
+   /* MARK: - SECTION 7: ZOOM MAP TO FIT ANNOTATIONS */
+   /* Given an array of annotations, calculate the 4 coordinates that encompass */
+   /* all of them in the map, give them a little padding on all sides, and move the map. */
    
-   // .....
+   func zoomToFitMapAnnotations(annotations: [GenericMapAnnotation]) {
+      guard annotations.count > 0 else {
+         return
+      }
+      
+      var topLeftCoord = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+      var bottomRightCoord = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+      
+      for annotation in annotations {
+         topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.location.longitude)
+         topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.location.latitude)
+         bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.location.longitude)
+         bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.location.latitude)
+      }
+      
+      var newRegion: MKCoordinateRegion = MKCoordinateRegion()
+      newRegion.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5
+      newRegion.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5
+      newRegion.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * annotationsZoomMargin
+      newRegion.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * annotationsZoomMargin
+      
+      self.moveMap(to: newRegion)
+      
+   }
    
-   func updateAnnotations(with selectedStop: CarrisNetworkModel.Stop) {
+   
+   
+   /* * */
+   /* MARK: - SECTION 8: UPDATE ANNOTATIONS WITH SELECTED CARRIS STOP */
+   /* Lorem ipsum dolor sit amet consectetur adipisicing elit. */
+   
+   func updateAnnotations(with activeStop: CarrisNetworkModel.Stop) {
       
       visibleAnnotations.removeAll()
       
       visibleAnnotations.append(
          GenericMapAnnotation(
-            id: Int(selectedStop.publicId) ?? 0,
-            location: CLLocationCoordinate2D(latitude: selectedStop.lat, longitude: selectedStop.lng),
-            item: .carris_stop(selectedStop)
+            id: activeStop.id,
+            location: CLLocationCoordinate2D(latitude: activeStop.lat, longitude: activeStop.lng),
+            item: .carris_stop(activeStop)
          )
       )
       
@@ -92,9 +154,9 @@ class MapController: ObservableObject {
    
    
    
-   /* MARK: - UPDATE ANNOTATIONS WITH SELECTED VARIANT */
-   
-   // .....
+   /* * */
+   /* MARK: - SECTION 9: UPDATE ANNOTATIONS WITH SELECTED CARRIS VARIANT */
+   /* Lorem ipsum dolor sit amet consectetur adipisicing elit. */
    
    func updateAnnotations(with activeVariant: CarrisNetworkModel.Variant) {
       
@@ -102,7 +164,7 @@ class MapController: ObservableObject {
          switch $0.item {
             case .carris_connection(_):
                return true
-            case .carris_stop(_), .carris_vehicle(_):
+            case .carris_vehicle(_), .carris_stop(_):
                return false
          }
       })
@@ -111,7 +173,7 @@ class MapController: ObservableObject {
          for connection in activeVariant.circularItinerary! {
             visibleAnnotations.append(
                GenericMapAnnotation(
-                  id: Int(connection.stop.publicId) ?? 0,
+                  id: connection.stop.id,
                   location: CLLocationCoordinate2D(latitude: connection.stop.lat, longitude: connection.stop.lng),
                   item: .carris_connection(connection)
                )
@@ -123,7 +185,7 @@ class MapController: ObservableObject {
          for connection in activeVariant.ascendingItinerary! {
             visibleAnnotations.append(
                GenericMapAnnotation(
-                  id: Int(connection.stop.publicId) ?? 0,
+                  id: connection.stop.id,
                   location: CLLocationCoordinate2D(latitude: connection.stop.lat, longitude: connection.stop.lng),
                   item: .carris_connection(connection)
                )
@@ -135,7 +197,7 @@ class MapController: ObservableObject {
          for connection in activeVariant.descendingItinerary! {
             visibleAnnotations.append(
                GenericMapAnnotation(
-                  id: Int(connection.stop.publicId) ?? 0,
+                  id: connection.stop.id,
                   location: CLLocationCoordinate2D(latitude: connection.stop.lat, longitude: connection.stop.lng),
                   item: .carris_connection(connection)
                )
@@ -143,15 +205,18 @@ class MapController: ObservableObject {
          }
       }
       
+      // Remove annotations with duplicate IDs (same stop on different itineraries)
+      visibleAnnotations.uniqueInPlace(for: \.id)
+      
       zoomToFitMapAnnotations(annotations: visibleAnnotations)
       
    }
    
    
    
-   /* MARK: - UPDATE ANNOTATIONS WITH VEHICLES LIST */
-   
-   // .....
+   /* * */
+   /* MARK: - SECTION 10: UPDATE ANNOTATIONS WITH ACTIVE CARRIS VEHICLES */
+   /* Lorem ipsum dolor sit amet consectetur adipisicing elit. */
    
    func updateAnnotations(with activeVehiclesList: [CarrisNetworkModel.Vehicle]) {
       
@@ -176,38 +241,5 @@ class MapController: ObservableObject {
       
    }
    
-   
-   
-   /* MARK: - ZOOM MAP TO FIT ANNOTATIONS */
-   
-   // ......
-   
-   func zoomToFitMapAnnotations(annotations: [GenericMapAnnotation]) {
-      guard annotations.count > 0 else {
-         return
-      }
-      
-      var topLeftCoord = CLLocationCoordinate2D(latitude: -90, longitude: 180)
-      var bottomRightCoord = CLLocationCoordinate2D(latitude: 90, longitude: -180)
-      
-      for annotation in annotations {
-         topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.location.longitude)
-         topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.location.latitude)
-         bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.location.longitude)
-         bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.location.latitude)
-      }
-      
-      // The margin on the sides of the annotations
-      let spanMargin = 1.7
-      
-      var newRegion: MKCoordinateRegion = MKCoordinateRegion()
-      newRegion.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5
-      newRegion.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5
-      newRegion.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * spanMargin
-      newRegion.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * spanMargin
-      
-      self.moveMap(to: newRegion)
-      
-   }
    
 }
