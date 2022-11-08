03ab5305a7f517e0aee249ff1b53a473e06cb798
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-11-08 00:02:46 +0000

    Stop Annotations are now always visible depending on the map region

--- GeoBus/App/Components/Map/MapAnnotations.swift
+++ GeoBus/App/Components/Map/MapAnnotations.swift
@@ -21,35 +21,11 @@ struct GenericMapAnnotation: Identifiable {
       case carris_stop(CarrisNetworkModel.Stop)
       case carris_connection(CarrisNetworkModel.Connection)
       case carris_vehicle(CarrisNetworkModel.Vehicle)
-      case ministop(CarrisNetworkModel.Stop)
+//      case ministop(CarrisNetworkModel.Stop)
    }
    
 }
 
-
-
-
-
-struct CarrisStopAnnotationView: View {
-   
-   public let stop: CarrisNetworkModel.Stop
-   
-   @ObservedObject private var sheetController = SheetController.shared
-   @ObservedObject private var carrisNetworkController = CarrisNetworkController.shared
-   
-   
-   var body: some View {
-      Button(action: {
-         TapticEngine.impact.feedback(.light)
-         carrisNetworkController.select(stop: self.stop)
-         sheetController.present(sheet: .carris_stopDetails)
-      }) {
-         StopIcon(isSelected: carrisNetworkController.activeStop?.id == self.stop.id)
-      }
-      .frame(width: 40, height: 40, alignment: .center)
-   }
-   
-}
 
 
 struct CarrisConnectionAnnotationView: View {
@@ -68,7 +44,7 @@ struct CarrisConnectionAnnotationView: View {
       Button(action: {
          TapticEngine.impact.feedback(.light)
          carrisNetworkController.select(connection: self.connection)
-         sheetController.present(sheet: .carris_connectionDetails)
+         sheetController.present(sheet: .ConnectionDetails)
       }) {
          StopIcon(
             orderInRoute: self.connection.orderInRoute,
@@ -102,7 +78,7 @@ struct CarrisVehicleAnnotationView: View {
       Button(action: {
          TapticEngine.impact.feedback(.light)
          carrisNetworkController.select(vehicle: vehicle.id)
-         sheetController.present(sheet: .carris_vehicleDetails)
+         sheetController.present(sheet: .VehicleDetails)
       }) {
          ZStack(alignment: .init(horizontal: .leading, vertical: .center)) {
             switch (vehicle.kind) {
@@ -124,37 +100,31 @@ struct CarrisVehicleAnnotationView: View {
 
 
 
-struct CarrisMiniStopAnnotationView: View {
+struct StopAnnotationView: View {
    
    public let stop: CarrisNetworkModel.Stop
    
-   @ObservedObject private var sheetController = SheetController.shared
-   @ObservedObject private var mapController = MapController.shared
-   @ObservedObject private var carrisNetworkController = CarrisNetworkController.shared
-   
-   
-   var icon: some View {
-      Button(action: {
-         TapticEngine.impact.feedback(.light)
-         carrisNetworkController.select(stop: self.stop)
-         sheetController.present(sheet: .carris_stopDetails)
-      }) {
-         Circle()
-            .foregroundColor(.blue)
-            .background(Color(.blue))
-      }
-      .frame(width: 15, height: 15, alignment: .center)
-   }
-   
+   @StateObject private var sheetController = SheetController.shared
+   @StateObject private var mapController = MapController.shared
+   @StateObject private var carrisNetworkController = CarrisNetworkController.shared
    
    var body: some View {
-      if (mapController.region.span.latitudeDelta < 0.0025 || mapController.region.span.longitudeDelta < 0.0025) {
-         icon
-      } else {
-         Circle()
-            .foregroundColor(.blue)
-            .background(Color(.blue))
-            .frame(width: 5, height: 5)
+      VStack {
+         if (carrisNetworkController.activeStop?.id == self.stop.id) {
+            StopIcon(style: .selected)
+         } else if (mapController.region.span.latitudeDelta < 0.0025 || mapController.region.span.longitudeDelta < 0.0025) {
+            StopIcon(style: .standard)
+         } else {
+            StopIcon(style: .mini)
+         }
+      }
+      .onTapGesture {
+         TapticEngine.impact.feedback(.light)
+         carrisNetworkController.select(stop: self.stop)
+         sheetController.present(sheet: .StopDetails)
+         withAnimation(.easeIn(duration: 0.5)) {
+            mapController.centerMapOnCoordinates(lat: self.stop.lat, lng: self.stop.lng)
+         }
       }
    }
    
